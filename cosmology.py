17409919b104ae4395a31bf880b66aa15ff1c113
Marcel Zemp <marcelzemp@gmail.com>
2014-01-22 08:36:17 +0000

    More physical constants added & synced with iof library.

--- cosmology.py
+++ cosmology.py
@@ -11,8 +11,55 @@ import scipy
 
 # Some constants
 
-km_per_s_2_kpc_per_Gyr = 1.02271216511
-G = 4.4985021530e-6 # kpc^3 Gyr^-2 Mo^-1
+# Exact definitions
+
+c_light = 299792458. # m s^{-1}
+au = 149597870700. # m
+yr = 31557600. # s (Julian year = 365.25 d)
+
+# Fundamental measured constants: central values
+
+G_Newton = 6.67384e-11 # m^3 s^{-2} kg^{-1} (NIST)
+k_Boltzmann = 1.3806488e-23 # J K^{-1} (NIST)
+uamu = 1.660538921e-27 # kg (NIST)
+m_proton = 1.672621777e-27 # kg (NIST)
+m_neutron =  1.674927351e-27 # kg (NIST)
+m_electron = 9.10938291e-31 # kg (NIST)
+eV = 1.602176565e-19 # J (NIST) 
+mu_sun = 1.32712440018e20 # m^3 s^{-2} (JPL)
+Mo = 1.98855e30 # kg (Wikipedia)
+Lo = 3.845e26 # W (Binney & Tremaine)
+
+# Fundamental measured constants: standard uncertainty
+
+sigma_G_Newton = 8e-15 # m^3 s^{-2} kg^{-1} (NIST)
+sigma_k_Boltzmann = 1.3e-29 # J K^{-1} (NIST)
+sigma_uamu = 7.3e-35 # kg (NIST)
+sigma_m_proton = 7.4e-35 # kg (NIST)
+sigma_m_neutron = 7.4e-35 # kg (NIST)
+sigma_m_electron = 4e-38 # kg (NIST)
+sigma_eV = 3.5e-27 # J (NIST)
+sigma_mu_sun = 8e9 # m^3 s^{-2} (JPL)
+sigma_Mo = 2.5e26 # kg (Wikipedia)
+sigma_Lo = 8e23 # W (Binney & Tremaine)
+
+# Derived constants: central values
+
+pc = au/numpy.tan(numpy.pi/(180*3600)) # m
+G_Newton_cosmology = mu_sun*pow(1e9*yr,2)/pow(1e3*pc,3) # kpc^3 Gyr^{-2} Mo^{-1}
+rho_crit = (3*pow(1e-1/pc,2))/(8*numpy.pi*G_Newton) # h^2 kg m^{-3}
+rho_crit_cosmology = (3*pow(1e8*yr/pc,2))/(8*numpy.pi*G_Newton_cosmology) # h^2 Mo kpc^{-3}
+km_per_s_2_kpc_per_Gyr = 1e9*yr/pc
+kpc_per_Gyr_2_km_per_s = pc/(1e9*yr)
+
+# Derived constants: standard uncertainty
+
+sigma_pc = 0 # m
+sigma_G_Newton_cosmology = sigma_mu_sun*pow(1e9*yr,2)/pow(1e3*pc,3) # kpc^3 Gyr^{-2} Mo^{-1}
+sigma_rho_crit = rho_crit*sigma_G_Newton/G_Newton # h^2 kg m^{-3}
+sigma_rho_crit_cosmology = rho_crit_cosmology*sigma_G_Newton_cosmology/G_Newton_cosmology # h^2 Mo kpc^{-3}
+sigma_km_per_s_2_kpc_per_Gyr = 0
+sigma_kpc_per_Gyr_2_km_per_s = 0
 
 # Functions for setting and printing cosmologial parameters
 
@@ -34,7 +81,7 @@ def set_cosmological_parameters(H0=70,OmegaB0=0.05,OmegaD0=0.25,OmegaL0=0.7,Omeg
 
     cp['OmegaM0'] = OmegaD0+OmegaB0
     cp['h0_100'] = H0/100.0
-    cp['rho_crit0'] = 277.53662719*pow(H0/100.0,2)
+    cp['rho_crit0'] = rho_crit_cosmology*pow(H0/100.0,2)
     cp['rho_mean0'] = cp['OmegaM0']*cp['rho_crit0']
 
     return cp
@@ -60,28 +107,33 @@ def print_cosmological_parameters(cp):
 # Cosmologial E(a) function
 
 def Ecosmo(a,cp):
+
     return numpy.sqrt(cp['OmegaR0']*pow(a,-4) + cp['OmegaM0']*pow(a,-3) + cp['OmegaK0']*pow(a,-2) + cp['OmegaL0'])
 
 # Cosmic time
 
 def tcosmic(a,cp):
+
     integrand = lambda x: 1/(x*Ecosmo(x,cp))
     return scipy.integrate.quad(integrand,0,a)[0]*1e3/cp['H0']/km_per_s_2_kpc_per_Gyr
 
 # Growth factor
 
 def D(a,cp):
+
     integrand = lambda x: 1/pow(x*Ecosmo(x,cp),3)
     return 2.5*cp['OmegaM0']*Ecosmo(a,cp)*scipy.integrate.quad(integrand,0,a)[0]
 
 # DC mode
 
 def Delta_DC(a,cp):
+
     return D(a,cp)*cp['Delta_DC0_NG']
 
 # abox in Eulerian view
 
 def abox_EV(a,cp):
+
     return a*pow(1+Delta_DC(a,cp),-1/3.0)
 
 # alpha-beta-gamma density profiles with 1. & 2. derivative
